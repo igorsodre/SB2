52760c40b44e13cc96b310beb7f0b911cfb40846
Patrícia Guimarães <deud.patricia@yahoo.com.br>
2017-06-10 16:15:05 -0300

    Add instrucoes_e_subrotinas

---
+++ instrucoes_e_subrotinas.asm
@@ -0,0 +1,39 @@
+; Instruções
+
+INPUT ;lê números inteiros com sinal
+call LeerInteiro
+
+OUTPUT ;escreve números inteiros com sinal
+call EscreverInteiro
+
+C_INPUT; lê caracteres ASCII
+
+C_OUTPUT; escreve caracteres ASCII
+
+S_INPUT; lê strings 
+
+S_OUTPUT; escreve strings
+
+H_INPUT; lê números hexadecimais
+
+H_OUTPUT; escreve números hexadecimais
+
+
+; Sub-rotinas
+
+LerInteiro ; 
+
+EscreverInteiro;
+
+LerChar;
+
+EscreverChar;
+
+LerString;
+
+EscreverString;
+
+LerHexa;
+
+EscreverHexa;
+
